790d92ac53d5a6cc3561f9f9473ecd39c1483ded
saBasKouw <36665806+saBasKouw@users.noreply.github.com>
2019-03-05 12:53:50 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -46,12 +46,10 @@ function clicked(d) {
     active.classed("active", false);
     active = d3.select(this).classed("active", true);
 
-    var element = active.node();
-    // use the native SVG interface to get the bounding box
+    let element = active.node();
 
     let bbox = element.getBBox();
-    let
-        dx = bbox.width,
+    let dx = bbox.width,
         dy = bbox.height,
         x = (bbox.x+(bbox.x+bbox.width)) / 2,
         y = (bbox.y+(bbox.y+bbox.height)) / 2,
@@ -60,8 +58,9 @@ function clicked(d) {
     d3.select("svg")
         .transition()
         .duration(750)
-        .style("stroke-width", 0.5 / scale + "px")
         .attr("transform", "scale(" + scale + ")translate(" + translate + ")");
+
+
 }
 
 function reset() {
@@ -74,51 +73,55 @@ function reset() {
         .attr("transform", "");
 }
 
+function genChart(myData){
+    let maxes = getAllMaxes(myData);
+    let mins = getAllMins(myData);
+
+    let scaleX = d3.scaleLinear()
+        .domain([mins["long"], maxes["long"]])
+        .range([0, mapWidth]);
+
+    let scaleY = d3.scaleLinear()
+        .domain([mins["lat"], maxes["lat"]])
+        .range([mapHeight,0]);
+
+    let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    let vis = d3.select("body").append("svg")
+        .attr("width", mapWidth)
+        .attr("height", mapHeight);
+
+    vis.selectAll("svg")
+        .data(myData.Polygons)
+        .enter().append("polygon")
+        .attr("points", function(d) {
+            return d.points.map(function(d) { return [scaleX(d.long),scaleY(d.lat)].join(","); }).join(" ");})
+        .attr("stroke", "white")
+        .attr("stroke-width", 1)
+        .attr("fill", function(d,i){return color(i);})
+        .on("mouseover", function(d) {
+            d3.select(this)
+                .transition()
+                .duration(50)
+                .style("opacity", .5)
+        })
+        .on("mouseout", function(d) {
+            d3.select(this)
+                .transition()
+                .duration(600)
+                .style("opacity", 1)
+
+        })
+        .on("click", clicked);
+}
+
 let myData ={"Polygons": []};
 
 for (let polygon of raw_polygons_district){
     myData.Polygons.push({"points": parsePolygon(polygon)});
 }
 
-let maxes = getAllMaxes(myData);
-let mins = getAllMins(myData);
-
-let scaleX = d3.scaleLinear()
-    .domain([mins["long"], maxes["long"]])
-    .range([0, mapWidth]);
-
-let scaleY = d3.scaleLinear()
-    .domain([mins["lat"], maxes["lat"]])
-    .range([mapHeight,0]);
-
-let color = d3.scaleOrdinal(d3.schemeCategory10);
-
-let vis = d3.select("body").append("svg")
-        .attr("width", mapWidth)
-        .attr("height", mapHeight);
-
-vis.selectAll("svg")
-    .data(myData.Polygons)
-    .enter().append("polygon")
-    .attr("points", function(d) {
-        return d.points.map(function(d) { return [scaleX(d.long),scaleY(d.lat)].join(","); }).join(" ");})
-    .attr("stroke", "white")
-    .attr("stroke-width", 2)
-    .attr("fill", function(d,i){return color(i);})
-    .on("mouseover", function(d) {
-        d3.select(this)
-            .transition()
-            .duration(50)
-            .style("opacity", .5)
-    })
-    .on("mouseout", function(d) {
-        d3.select(this)
-            .transition()
-            .duration(800)
-            .style("opacity", 1)
-
-    })
-    .on("click", clicked);
+genChart(myData);
 
 
 
