50c9e0241e947e2b3df77e6fc898545baf5d235f
saBasKouw <36665806+saBasKouw@users.noreply.github.com>
2019-03-13 18:08:50 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -5,12 +5,15 @@ let active = d3.select(null);
 let maxes;
 let mins;
 let vis;
-
+let myData;
 let scaleX;
 let scaleY;
 let level = "city";
 let parent;
 let child;
+
+let currentOpacity;
+
 
 function parsePolygon(polygon){
     polygon = polygon.replace("POLYGON((", "");
@@ -58,14 +61,13 @@ function whatIsClicked(d){
     let keys = Object.keys(d);
 
     if (keys.indexOf("district") >=0){
-        level = "district";
         return "district";
     } else if (keys.indexOf("neighbourhood") >=0){
-        level = "neighbourhood";
         return "neighbourhood";
     }
 }
 function reset() {
+    level = "city";
     active.classed("active", false);
     active = d3.select(null);
 
@@ -89,55 +91,88 @@ function reset() {
         .duration(750)
         .style("stroke-width", "2px")
         .attr("transform", "");
+
+    othersBack();
+
 }
 
+function othersGone(d){
+    if(level === "city"){
+        for(let poly of myData){
+            d3.select("#"+poly.district)
+                .transition()
+                .duration(400)
+                .style("opacity", 0.2)
+        }
+    } else if(level === "district" || level === "neighbourhood"){
+        for(let poly of child){
+            d3.select("#"+poly.neighbourhood)
+                .transition()
+                .duration(400)
+                .style("opacity", 0.2)
+        }
+        d3.select("#"+d.neighbourhood)
+            .transition()
+            .duration(400)
+            .style("opacity", 1)
+    }
+
+}
+
+function othersBack(){
+    for(let poly of myData){
+        d3.select("#"+poly.district)
+            .transition()
+            .duration(400)
+            .style("opacity", 1)
+    }
+}
 
 
 function clicked(d) {
 
-    console.log(d);
-    console.log(level, whatIsClicked(d));
+    if(!(level === "district" && whatIsClicked(d) === "district") &&
+        !(level === "neighbourhood" && whatIsClicked(d) === "district")){
+        //Zooms in on center of polygon
+        if (active.node() === this){
+            return reset();
+        } else {
+            if (whatIsClicked(d) === "district"){
+                child = d.neighbourhoods;
+                parent = "#"+d.district;
+                d3.select("#"+d.district)
+                    .transition()
+                    .duration(50)
+                    .style("opacity", 0);
+                drawHoodPolygons(d);
 
-    //Zooms in on center of polygon
-    if (active.node() === this){
-        level = "city";
-        return reset();
-    } else {
-        if (whatIsClicked(d) === "district"){
-            child = d.neighbourhoods;
-            parent = "#"+d.district;
-            d3.select("#"+d.district)
-                .transition()
-                .duration(50)
-                .style("opacity", 0);
-            drawHoodPolygons(d);
-        } else if(whatIsClicked(d) === "neighbourhood"){
+            } else if(whatIsClicked(d) === "neighbourhood"){
 
+            }
         }
 
+        othersGone(d);
+        active.classed("active", false);
+        active = d3.select(this).classed("active", true);
+        let element = active.node();
 
 
-        // console.log(d);
+        let bbox = element.getBBox();
+        let dx = bbox.width,
+            dy = bbox.height,
+            x = (bbox.x+(bbox.x+bbox.width)) / 2,
+            y = (bbox.y+(bbox.y+bbox.height)) / 2,
+            scale = .6 / Math.max(dx / mapWidth, dy / mapHeight),
+            translate = [mapWidth/ 2 - x, mapHeight/ 2 - y];
+        d3.select("svg")
+            .transition()
+            .duration(750)
+            .attr("transform", "scale(" + scale + ")translate(" + translate + ")");
+
+    }else{
+        return reset();
     }
-
-
-    active.classed("active", false);
-    active = d3.select(this).classed("active", true);
-    let element = active.node();
-
-
-    let bbox = element.getBBox();
-    let dx = bbox.width,
-        dy = bbox.height,
-        x = (bbox.x+(bbox.x+bbox.width)) / 2,
-        y = (bbox.y+(bbox.y+bbox.height)) / 2,
-        scale = .9 / Math.max(dx / mapWidth, dy / mapHeight),
-        translate = [mapWidth/ 2 - x, mapHeight/ 2 - y];
-    d3.select("svg")
-        .transition()
-        .duration(750)
-        .attr("transform", "scale(" + scale + ")translate(" + translate + ")");
-
+    level = whatIsClicked(d);
 }
 
 
@@ -150,7 +185,7 @@ function drawHoodPolygons(d){
         .attr("points", function(d) {
             return d.polygon.map(function(d) { return [scaleX(d.long),scaleY(d.lat)].join(","); }).join(" ");})
         .attr("stroke", "white")
-        .attr("stroke-width", 1)
+        .attr("stroke-width", 0.7)
         .attr("fill", function(d,i){return color(i);})
         // .on("mouseover", function(d) {
         //     d3.select(this)
@@ -179,7 +214,7 @@ function drawDistrictPolygons(data){
         .attr("points", function(d) {
             return d.polygon.map(function(d) { return [scaleX(d.long),scaleY(d.lat)].join(","); }).join(" ");})
         .attr("stroke", "white")
-        .attr("stroke-width", 1)
+        .attr("stroke-width", 0.7)
         .attr("fill", function(d,i){return color(i);})
         // .on("mouseover", function(d) {
         //     d3.select(this)
@@ -228,7 +263,7 @@ function createDictionary(data, otherData){
 
 function initializeChart(data, otherData){
 
-    let myData =createDictionary(data, otherData);
+    myData =createDictionary(data, otherData);
     maxes = getAllMaxes(myData);
     mins = getAllMins(myData);
 
